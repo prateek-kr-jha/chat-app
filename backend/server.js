4767dde912bfd5d7489c277026e8a4caed840f28
Prateek Kr Jha <prateekj1211@gmail.com>
2024-02-28 22:37:19 +0530

    added nodemon and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const data = require('./data/data');
-console.log(data);
+const dotenv = require("dotenv");
+// console.log(data);
 const app = express();
+dotenv.config();
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send("Api is running");
+    res.send("Api is running!!!");
 });
 
-app.get('/api/chat/', (req, res) => {
+app.get('/api/chat', (req, res) => {
     res.send(data)
 })
 
-app.listen(5000, console.log("Server Started at PORT 5000"))
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params.id);
+    const singleChat = data.find(c => {
+        return c._id == req.params.id;
+    })
+
+    res.send(singleChat);
+})
+app.listen(port, console.log(`Server Started at PORT ${port}`))
