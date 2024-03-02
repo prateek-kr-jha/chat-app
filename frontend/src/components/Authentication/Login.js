abaadb034a972b000c2553c70fc6022d3d75e4c8
Prateek Kr Jha <prateekj1211@gmail.com>
2024-03-03 03:04:55 +0530

    created login page

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FormControl, Input, VStack, FormLabel, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 
 const Login = () => {
+    
+    const [show, setShow] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = () => setShow(!show);
+  const submitHandler = () => {};
   return (
-    <div>
-      
-    </div>
+    <VStack spacing="5px" color="black">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input 
+            placeholder='Enter Your Email'
+            onChange={(e)=>setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+            <Input 
+            type={show?"text":"password"}
+                placeholder='Enter Your Password'
+                onChange={(e)=>setPassword(e.target.value)}
+            />
+            <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? "Hide" : "Show"}
+                </Button>
+            </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <Button
+        colorScheme='blue'
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+      <Button
+      variant="solid"
+        colorScheme='red'
+        width="100%"
+        // style={{ marginTop: 15 }}
+        onClick={() => {
+            setEmail("guest@exampl.com");
+            setPassword("123456");
+        }}
+      >
+        Get Guest User Credentials
+      </Button>
+    </VStack>
   )
 }
 
