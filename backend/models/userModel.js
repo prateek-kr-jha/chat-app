13ffc32a97c88d91148d78a5ab3ead08f8b7627f
Prateek Kr Jha <prateekj1211@gmail.com>
2024-03-03 21:53:22 +0530

    created registerUser

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,12 @@
-const mongoose = requirE("mongoose");
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
-        email: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         pic: {
             type: String,
-            required: true,
             default: ""
         },
     },
--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,18 @@ const data = require('./data/data');
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require('colors');
+const userRoutes = require("./routes/userRoutes");
 // console.log(data);
 const app = express();
 dotenv.config();
 connectDB();
 
 const port = process.env.PORT || 4000;
-
+app.use(express.json()); // to accept JSON data
 app.get('/', (req, res) => {
     res.send("Api is running!!!");
 });
 
-app.get('/api/chat', (req, res) => {
-    res.send(data)
-})
+app.use('/api/user', userRoutes);
 
-app.get('/api/chat/:id', (req, res) => {
-    console.log(req.params.id);
-    const singleChat = data.find(c => {
-        return c._id == req.params.id;
-    })
-
-    res.send(singleChat);
-})
 app.listen(port, console.log(`Server Started at PORT ${port}`.yellow.bold))
